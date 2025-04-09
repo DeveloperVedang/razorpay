740f39dd1b2bb0d7aa3af2fb022391d16a426c55
Developer Vedang <developer.vedang@gmail.com>
2025-04-09 19:12:26 +0530

    Add files via upload

---
+++ routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Helpers\ExtensionHelper;
+
+Route::post('/razorpay/webhook', [App\Extensions\Gateways\RazorPay\RazorPay::class, 'webhook'])->name('razorpay.webhook');
+Route::get('/razorpay/payment/{id}/{order_amount}/{invoiceId}/{key_id}', function ($id, $order_amount, $invoiceId, $key_id) {
+    return view('RazorPay::payment', compact('id', 'order_amount', 'invoiceId', 'key_id'));
+})->name('razorpay.payment');
+Route::post('/razorpay/callback/{invoiceId}', function ($invoiceId) {
+    return redirect()->route('clients.invoice.show', $invoiceId);
+})->name('razorpay.callback');
+Route::get('/razorpay/cancel/{invoiceId}', function ($invoiceId) {
+
+    return redirect()->route('clients.invoice.show', $invoiceId);
+
+})->name('razorpay.cancel');
